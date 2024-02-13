bc1fad9b29d0683ce4b575c3ffbf403c2ad7055d
Lee-Young-Jae <ori_@kakao.com>
2024-02-13 21:54:56 +0900

    fix: 보드의 크기보다 큰 승리조건을 고를 수 있던 문제를 해결한다.

--- src/Pages/Setting/index.tsx
+++ src/Pages/Setting/index.tsx
@@ -1,20 +1,31 @@
 import { useContext } from "react";
 import { GameSettingsContext } from "../../context/GameSettingsContext";
-import { GAME_RULES } from "../../utills/constance/gameSetting";
 import SettingPlayer from "./component/SettingPlayer";
 
 import { useNavigate } from "react-router-dom";
+
+import S from "./Style";
 
 const Setting = () => {
   const navigate = useNavigate();
   const { settings, updateSettings } = useContext(GameSettingsContext);
 
-  const onChangeBoardSize = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateSettings({ ...settings, boardSize: parseInt(e.target.value) });
+  const onChangeBoardSize = (size: number) => {
+    if (settings.winCondition > size) {
+      updateSettings({ ...settings, winCondition: size, boardSize: size });
+      return;
+    }
+    updateSettings({
+      ...settings,
+      boardSize: size,
+    });
   };
 
-  const onChangeWinCondition = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateSettings({ ...settings, winCondition: parseInt(e.target.value) });
+  const onChangeWinCondition = (e: React.MouseEvent<HTMLButtonElement>) => {
+    updateSettings({
+      ...settings,
+      winCondition: parseInt(e.currentTarget.value),
+    });
   };
 
   const onChangeStartingPlayer = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -29,51 +40,70 @@ const Setting = () => {
   };
 
   return (
-    <div>
-      <h1>Setting</h1>
-      <div>
-        <label>
-          Board Size
-          <input
-            type="number"
-            max={GAME_RULES.MAX_BOARD_SIZE}
-            min={GAME_RULES.MIN_BOARD_SIZE}
-            value={settings.boardSize}
-            onChange={onChangeBoardSize}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Win Condition
-          <input
-            type="number"
-            max={GAME_RULES.MAX_WIN_CONDITION}
-            min={GAME_RULES.MIN_WIN_CONDITION}
-            value={settings.winCondition}
-            onChange={onChangeWinCondition}
-          />
-        </label>
-      </div>
-      <SettingPlayer currentPlayer="player1"></SettingPlayer>
-      <SettingPlayer currentPlayer="player2"></SettingPlayer>
+    <S.Container>
+      <S.SettingBoardSize>
+        <S.Label>얼마나 큰 게임판을 생성할까요?</S.Label>
+        <S.BoardSizeButton
+          onClick={() => onChangeBoardSize(3)}
+          className={settings.boardSize === 3 ? "active" : ""}
+        >
+          3X3
+        </S.BoardSizeButton>
+        <S.BoardSizeButton
+          onClick={() => onChangeBoardSize(4)}
+          className={settings.boardSize === 4 ? "active" : ""}
+        >
+          4X4
+        </S.BoardSizeButton>
+        <S.BoardSizeButton
+          onClick={() => onChangeBoardSize(5)}
+          className={settings.boardSize === 5 ? "active" : ""}
+        >
+          5X5
+        </S.BoardSizeButton>
+      </S.SettingBoardSize>
 
-      <div>
-        <label>
-          Starting Player
-          <select
-            value={settings.startingPlayer}
-            onChange={onChangeStartingPlayer}
+      <S.SettingWinCondition>
+        <S.Label>몇 개의 돌을 연달아 놓으면 승리할까요?</S.Label>
+        {new Array(3).fill(0).map((_, index) => (
+          <S.WinConditionButton
+            key={index}
+            value={index + 3}
+            disabled={index + 3 > settings.boardSize}
+            className={
+              settings.winCondition === index + 3 ? "active" : undefined
+            }
+            onClick={onChangeWinCondition}
           >
-            <option value="player1">Player 1</option>
-            <option value="player2">Player 2</option>
-            <option value="random">Random</option>
-          </select>
-        </label>
-      </div>
+            {index + 3}
+          </S.WinConditionButton>
+        ))}
+      </S.SettingWinCondition>
 
-      <button onClick={onClickGameStart}>GameStart!</button>
-    </div>
+      <S.SettingPlayer>
+        <S.Label>플레이어 정보를 입력해주세요.</S.Label>
+        <S.Players>
+          <SettingPlayer currentPlayer="player1"></SettingPlayer>
+          <SettingPlayer currentPlayer="player2"></SettingPlayer>
+        </S.Players>
+      </S.SettingPlayer>
+
+      <S.SettingPlayer>
+        <S.Label>누가 먼저 시작할까요?</S.Label>
+        <S.Select
+          value={settings.startingPlayer}
+          onChange={onChangeStartingPlayer}
+        >
+          <S.Option value="player1">Player 1</S.Option>
+          <S.Option value="player2">Player 2</S.Option>
+          <S.Option value="random">Random</S.Option>
+        </S.Select>
+      </S.SettingPlayer>
+
+      <S.GameStartButton onClick={onClickGameStart}>
+        GameStart!
+      </S.GameStartButton>
+    </S.Container>
   );
 };
 
