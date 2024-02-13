71d9219a9a915624cb75fa3d188f642620b37abb
Lee-Young-Jae <ori_@kakao.com>
2024-02-13 18:40:00 +0900

    feat: 게임 시작 / 기록된 게임 보기 페이지로 이동 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Main from "./Pages/Main";
+import Setting from "./Pages/Setting";
+import Play from "./Pages/Play";
+import History from "./Pages/History";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/play" element={<Play />} />
+          <Route path="history" element={<History />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
