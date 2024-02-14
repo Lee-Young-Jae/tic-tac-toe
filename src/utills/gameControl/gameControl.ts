69b5f9368f4dbd3470cfd16cc2bb341bb770a7cf
Lee-Young-Jae <ori_@kakao.com>
2024-02-14 10:22:08 +0900

    feat: 게임의 승패여부를 판별하는 checkWin 함수를 작성한다.

---
+++ src/utills/gameControl/gameControl.ts
@@ -0,0 +1,82 @@
+import { GameState } from "../../types/game";
+
+export const checkWin = (
+  board: GameState["board"],
+  player: string,
+  winCondition: number
+) => {
+  const boardSize = board.length;
+
+  // row확인
+  for (let y = 0; y < boardSize; y++) {
+    let count = 0;
+    for (let x = 0; x < boardSize; x++) {
+      if (board[y][x]?.mark === player) {
+        count++;
+        if (count === winCondition) {
+          return true;
+        }
+      } else {
+        count = 0;
+      }
+    }
+  }
+
+  // column 확인
+  for (let x = 0; x < boardSize; x++) {
+    let count = 0;
+    for (let y = 0; y < boardSize; y++) {
+      if (board[y][x]?.mark === player) {
+        count++;
+        if (count === winCondition) {
+          return true;
+        }
+      } else {
+        count = 0;
+      }
+    }
+  }
+
+  // 대각선 확인
+  for (let y = 0; y < boardSize; y++) {
+    for (let x = 0; x < boardSize; x++) {
+      let count = 0;
+      for (let i = 0; i < winCondition; i++) {
+        if (y + i < boardSize && x + i < boardSize) {
+          if (board[y + i][x + i]?.mark === player) {
+            count++;
+            if (count === winCondition) {
+              return true;
+            }
+          } else {
+            count = 0;
+          }
+        }
+      }
+    }
+  }
+
+  for (let y = 0; y < boardSize; y++) {
+    for (let x = 0; x < boardSize; x++) {
+      let count = 0;
+      for (let i = 0; i < winCondition; i++) {
+        if (y + i < boardSize && x - i >= 0) {
+          if (board[y + i][x - i]?.mark === player) {
+            count++;
+            if (count === winCondition) {
+              return true;
+            }
+          } else {
+            count = 0;
+          }
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+export const checkDraw = (board: GameState["board"]) => {
+  return board.every((row) => row.every((cell) => cell));
+};
