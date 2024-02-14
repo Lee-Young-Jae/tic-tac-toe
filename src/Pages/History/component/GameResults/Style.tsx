0e1c6e40fd3aefdbc613e5a4f42c48207e8f0fd2
Lee-Young-Jae <ori_@kakao.com>
2024-02-14 20:32:47 +0900

    style: 기록이 없을 때, 대체 텍스트를 표시하도록 한다.

--- src/Pages/History/component/GameResults/Style.tsx
+++ src/Pages/History/component/GameResults/Style.tsx
@@ -6,10 +6,57 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 20px;
+
+  max-height: 700px;
+  overflow-y: auto;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+  }
+`;
+
+export const PlayGameButton = styled.button`
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1rem;
+  border: none;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #e6e6e6;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`;
+
+export const ResultItem = styled.li`
+  cursor: pointer;
+  margin: 10px 0;
+  padding: 10px;
+  border-radius: 5px;
+  width: 100%;
+  box-sizing: border-box;
+  text-align: center;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.2);
+
+  &:hover {
+    background-color: #f2f2f2;
+  }
 `;
 
 const S = {
   Container,
+  ResultItem,
+  PlayGameButton,
 };
 
 export default S;
--- src/Pages/History/component/GameResults/index.tsx
+++ src/Pages/History/component/GameResults/index.tsx
@@ -11,11 +11,24 @@ interface GameResultsProps {
 const GameResults = ({ gameResults }: GameResultsProps) => {
   const { openModal, closeModal } = useModal();
 
+  if (gameResults.length === 0) {
+    return (
+      <S.Container>
+        <h2>기록이 없습니다.</h2>
+        <p>게임을 플레이하고 기록을 남겨보세요!</p>
+
+        <S.PlayGameButton>
+          <a href="/setting">게임하러 가기</a>
+        </S.PlayGameButton>
+      </S.Container>
+    );
+  }
+
   return (
     <S.Container>
       <ul>
         {gameResults.map((gameResult, index) => (
-          <li
+          <S.ResultItem
             key={index}
             onClick={() => {
               openModal(
@@ -26,9 +39,17 @@ const GameResults = ({ gameResults }: GameResultsProps) => {
               );
             }}
           >
-            {gameResult.player1.mark} vs {gameResult.player2.mark}
+            <p>
+              <span style={{ color: gameResult.player1.color }}>
+                {gameResult.player1.mark}
+              </span>{" "}
+              vs{" "}
+              <span style={{ color: gameResult.player2.color }}>
+                {gameResult.player2.mark}
+              </span>
+            </p>
             {timeAgo(gameResult.date)}
-          </li>
+          </S.ResultItem>
         ))}
       </ul>
     </S.Container>
